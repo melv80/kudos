96e7aed412210e393b14ae79fe511d5760abfd40
krissi <kristianjantz@gmail.com>
2021-04-05 20:46:08 +0200

    user roles

--- src/main/java/com/kudos/server/config/WebSecurityConfig.java
+++ src/main/java/com/kudos/server/config/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers(  "/login", "/css/**", "/js/**").permitAll()
+				.antMatchers(  "/login", "/css/**", "/js/**", "/icons/**").permitAll()
                 .anyRequest().authenticated()
 				.and()
 			.formLogin()
